a01d6f614744c7e2b0fee1554ed31c06b65e5eee
Tyler Buck <tylerbuck09@gmail.com>
2024-12-31 14:16:30 -0500

    Removing default instance_key

--- iac/variables.tf
+++ iac/variables.tf
@@ -35,7 +35,7 @@ variable "number_of_instances" {
 variable "instance_key" {
   type        = string
   description = "Secure key pair to use for SSH access into the EC2 instance(s)."
-  default     = "treebux-ec2-a"
+  default     = ""
   validation {
     condition     = length(var.instance_key) > 0
     error_message = "An instance key must be a declared."
@@ -48,12 +48,15 @@ variable "user_data" {
     #!/usr/bin/bash
     sudo apt-get update
 
+    # Switch out of root user
+    su - ubuntu
+
     # Install Docker
     ## Per Docker's instructions: https://docs.docker.com/en  gine/install/ubuntu/
     echo "*** Installing Docker"
     # Add Docker's official GPG key:
-    apt-get -y update
-    apt-get -y install ca-certificates curl
+    sudo apt-get -y update
+    sudo apt-get -y install ca-certificates curl
     install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     chmod a+r /etc/apt/keyrings/docker.asc
@@ -63,8 +66,8 @@ variable "user_data" {
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
       $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get -y update
-    apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get -y update
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     if docker run hello-world | grep -q 'This message shows that your installation appears to be working correctly.'; then echo "*** Docker successfully installed"; else echo "*** ERROR: Docker failed to install"; fi
     echo "*** Completed Installing Docker"
 
